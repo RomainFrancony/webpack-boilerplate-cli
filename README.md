500f3cb99c3277afa390ad7e678eb4bb4fb20ca5
FRANCONY Romain <r.francony@gmail.com>
2017-12-02 12:03:27 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Webpack boilerplate CLI
+
+> A simple CLI creating webpack project using boilerplate
+
+Simple CLI creating webpack project using [gweltaz-calori/webpack-boilerplate](https://github.com/gweltaz-calori/webpack-boilerplate).
+
+This project is mainly inspired by [vue-cli](https://github.com/vuejs/vue-cli).
+
+### Installation
+
+Prerequisites: [Node.js](https://nodejs.org/en/) and [Git](https://git-scm.com/).
+
+**The package is no available on npm yet**
+
+``` bash
+npm install -g webpack-boilerplate-cli
+```
+
+
+### Usage
+
+``` bash
+webpack-init <project-name>
+```
+
+Example:
+
+``` bash
+webpack-boilerplate-cli my-project
+```
+
+The above command pulls the [gweltaz-calori/webpack-boilerplate](https://github.com/vuejs-templates/webpack) repository, and generates the project at `./my-project/`.
+
+### What's next
+
+* Prompt question about the project in order to change the ```packages.json``` (name, description, author, ...)
+
+### Contributing
+
+If you want a new feature to be included, open a issue with the feature request label.
+
+Help is welcome ! If you want to create new feature or fix bug, fork this repo and open a pull request.
+
+
+### License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ bin/webpack-init.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+const download = require('download-git-repo');
+const ora = require('ora');
+const path = require('path');
+const chalk = require('chalk');
+const program = require('commander');
+const inquirer = require('inquirer');
+const exists = require('fs').existsSync;
+const logger = require('../lib/logger');
+
+const template = "gweltaz-calori/webpack-boilerplate";
+
+/**
+ * Usage.
+ */
+program.usage('[project-name]');
+
+/**
+ * Help.
+ */
+program.on('--help', () => {
+    console.log('  Usage:');
+    console.log();
+    console.log(chalk.gray('    # create a new project with custom name'));
+    console.log('    $ webpack init my-project');
+    console.log();
+});
+
+/**
+ * Settings.
+ */
+let rawName = "webpack-boilerplate";
+
+program.parse(process.argv);
+
+if (program.args) {
+    rawName = program.args[0];
+}
+const inPlace = !rawName || rawName === '.';
+const name = inPlace ? path.relative('../', process.cwd()) : rawName;
+const to = path.resolve(rawName || '.');
+
+/**
+ * Padding.
+ */
+console.log();
+process.on('exit', () => {
+    console.log();
+});
+
+/**
+ * Check if the destination folder already exists
+ */
+if (exists(to)) {
+    inquirer.prompt([{
+        type: 'confirm',
+        message: inPlace ? 'Generate project in current directory ?' : 'Target directory exists. Continue ?',
+        name: 'ok'
+    }]).then(answers => {
+        if (answers.ok) {
+            downloadBoilerplate();
+        }
+    }).catch(logger.fatal);
+} else {
+    downloadBoilerplate();
+}
+
+
+/**
+ * Download git repo into destination folder
+ */
+function downloadBoilerplate() {
+    const spinner = ora('downloading boilerplate');
+    spinner.start();
+
+    download(template, to, {}, err => {
+        spinner.stop();
+
+        if (err) {
+            logger.fatal('Failed to download repo ' + template + ': ' + err.message.trim());
+        }
+        logger.success('Generated "%s".', name)
+    });
+}
